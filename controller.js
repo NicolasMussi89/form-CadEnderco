7b1dd8dc975d6ab6efe9047e784e2b056e1c3bab
Nicolas <nmussidasilva@gmail.com>
2023-09-29 12:08:18 -0300

    add erro

--- controller.js
+++ controller.js
@@ -1,15 +1,37 @@
-'use strict';//Modo restrito hj
-//Consumindo API de CEP, do ViaCep
-//https://viacep.com.br/
-
-//Função para limpar o formulário
+// Consumindo API de CEP, do Viacep
+// https://viacep.com.br/
+ 
+// Função para limpar o formulário
 const limparFormulario = () => {
-    document.getElementById('rua').value = '';
-    document.getElementById('bairro').value = '';
-    document.getElementById('cidade').value = '';
-    document.getElementById('estado').value = '';
+    document.getElementById('rua').value ='';
+    document.getElementById('bairro').value ='';
+    document.getElementById('cidade').value ='';
+    document.getElementById('estado').value ='';
 }
-
-//verifica se CEP é valido
-const eNumero = (numero) => /^[0-9]+$/.test(numero);//testa número informado com expressão regular
+ 
+// Verifica se Cep é valido
+const eNumero = (numero) => /^[0-9]+$/.test(numero); //testa número informado com expresão regular
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);// Verifica tamanho do cep digite e executa função de validação do cep eNumero
+ 
+//função para preencher formulario
+const preencherFormulario = (endereco) => {
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = enderco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+ 
+//Consumo da API da ViaCEp
+const pesquisarCep = async() => {
+    limparFormulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+ 
+    if(cepValido(cep.value)){
+       const dados = await fetch(url); //esperar
+       const addres = await dados.json();
+
+       if(addres.hasOwnProperty('erro')){
+        
+       }
+    }
+}
